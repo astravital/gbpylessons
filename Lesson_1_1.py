ca5ae211ab3e4544848a26396823ed7e8e52b6a1
astravital <66038311+astravital@users.noreply.github.com>
2020-05-29 15:20:55 +0600

    Create Lesson_1_1.py

---
+++ Lesson_1_1.py
@@ -0,0 +1,11 @@
+r = 5
+p = 3.14
+color = 'red'
+print('r = ', r, '; p = ', p, '; color = ', color)
+print('')
+a = input('Input a - ')
+print(a)
+b = input('Input b - ')
+print(b)
+c = input('Input c - ')
+print(c)
